75ded05354a2423ea22b4d48d6933e6afb41373b
das-cmu <rishavdas7@gmail.com>
2018-05-21 21:48:18 -0700

    Re-ordering create and save

--- mealscountserver/dao.py
+++ mealscountserver/dao.py
@@ -3,8 +3,8 @@ from models import MC_REQUEST, MC_SCHOOL
 
 def upload(mealCountsSchool, inputEmail):
 	requestDbObject= MC_REQUEST(request_time =datetime.datetime.now(), pending = False, email = inputEmail)
-	schoolDbObject = MC_SCHOOL(school_name = mealCountsSchool.schoolName, total_number_of_students = mealCountsSchool.numTotalStudents, identified_student_population = mealCountsSchool.numIdentifiedStudents, request = requestDbObject)
 	requestDbObject.save()
+	schoolDbObject = MC_SCHOOL(school_name = mealCountsSchool.schoolName, total_number_of_students = mealCountsSchool.numTotalStudents, identified_student_population = mealCountsSchool.numIdentifiedStudents, request = requestDbObject)
 	schoolDbObject.save()
 
 # See mealcountsschool.py for a definition of objects in the mealCountsSchoolsArray
