36d4aaf222908cbbdbf97eb3ed7da4249b6b4c7d
Nikolaj Baer <nikolaj.baer@gmail.com>
2019-04-22 16:56:05 -0700

    clear out for validation branch, just focus on algorithm and data eval

--- mealscount/README.md
+++
@@ -1,2 +0,0 @@
-mealscount
-Optimizing a free-meal reimbursement program for K-12 schools
--- mealscountenv3/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/rishav/mealscount/mealscountenv3/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
