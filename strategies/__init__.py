9dbff242d6cac2c592249d229f32e23d09bba921
Harrison <granthwagener@gmail.com>
2019-04-29 14:21:08 -0700

    added exhaustive strategy-some debugging still needed

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -1,12 +1,14 @@
 from .base import OneToOneCEPDistrict,OneGroupCEPDistrict 
 from .mc_algo_v2 import AlgoV2CEPDistrict
 from .binning import BinCEPDistrict
+from .exhaustive import ExaustiveCEPDistrict
 
 STRATEGIES = {
     "OneToOne":OneToOneCEPDistrict,
     "OneGroup":OneGroupCEPDistrict,
     "Binning":BinCEPDistrict,
     "AlgoV2":AlgoV2CEPDistrict,
+    "Exhaustive":ExaustiveCEPDistrict
 }
 
 
--- strategies/exhaustive.py
+++ strategies/exhaustive.py
@@ -1,6 +1,6 @@
 from .base import BaseCEPDistrict,CEPGroup
 
-class BinCEPDistrict(BaseCEPDistrict):
+class ExaustiveCEPDistrict(BaseCEPDistrict):
     ''' Grouping strategy is to compute every possible partition
 
     * This follows with https://en.wikipedia.org/wiki/Partition_of_a_set
@@ -9,10 +9,43 @@ class BinCEPDistrict(BaseCEPDistrict):
 
       '''
     def create_groups(self):
-        raise NotImplemented("Some more reading is required..") 
-        # For every possible partition:
-        #   generate it
-        #   if best so far, keep
-        # cont.
-        # Note compute best as total ISP (possibly later look at reimbursement level)
+        # some debugging/optimization required
+
+        # check district size, if over 10 don't calculates partitions
+        # bell number of 10 = 115,975 (15 ~ 1.4 bil;  20 = 51.7 tril)
+        # if over assign groups like OneToOne (this way it can run on all districts without taking a year)
+        if len(self.schools) > 10:
+            self.groups = [
+                CEPGroup(school.district,school.name,[school])
+                for school in self.schools
+            ]
+        else:
+            def partition(collection):
+                # function straight from stock overflow- seems to work well (search Set partitions in python)
+                if len(collection) == 1:
+                    yield [collection]
+                    return
+
+                first = collection[0]
+                for smaller in partition(collection[1:]):
+                    # insert `first` in each of the subpartition's subsets
+                    for n, subset in enumerate(smaller):
+                        yield smaller[:n] + [[first] + subset] + smaller[n + 1:]
+                    # put `first` in its own subset
+                    yield [[first]] + smaller
+
+            best_grouping = []
+            best_covered = 0
+            # generate all partions
+            for x in partition(self.schools):
+                # save grouping with highest num of covered students
+                grouping = []
+                students_covered = 0
+                for i, group in enumerate(x):
+                    grouping.append(CEPGroup(self, i, group))
+                    students_covered += grouping[i].covered_students()
+                if students_covered > best_covered:
+                    best_grouping = grouping
+                    best_covered = students_covered
+            self.groups = best_grouping
 
