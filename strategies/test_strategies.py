d93523cfe232eabe9e16cee1612a7e9d8cf088fd
Nikolaj Baer <nikolaj.baer@gmail.com>
2019-10-27 13:58:19 -0700

    added basic test for cep objects

--- strategies/test_strategies.py
+++ strategies/test_strategies.py
@@ -6,11 +6,8 @@ SCHOOL_DATA_COLS = ["District Code","School Name","School Code",\
                     "foster","homeless","migrant","direct_cert",\
                     "frpm","total_enrolled"]
 
-class StrategyTestCase(unittest.TestCase):
-    def setUp(self):
-        self.test_districts = []
-        self.create_default_districts()
-
+class CEPTestMixin(object):
+    '''Mixin for reusable school / district building code for tests''' 
     def mk_school(self,district,code,isp,total_enrolled):
         return CEPSchool({
             "District Name":district.name,
@@ -27,7 +24,46 @@ class StrategyTestCase(unittest.TestCase):
         basic.schools.append(self.mk_school(basic,"A",0.625,1000))
         basic.schools.append(self.mk_school(basic,"B",0.40,1000))
         basic.schools.append(self.mk_school(basic,"C",0.25,1000))
-        self.test_districts.append(basic)
+        return basic
+ 
+class CEPTestCase(unittest.TestCase,CEPTestMixin):
+    '''Tests basic python data model (CEPSchool,CEPDistrict,CEPGroup)'''
+
+    def test_district(self):
+        district = self.create_default_districts()
+        self.assertEqual( district.total_enrolled, sum([s.total_enrolled for s in district.schools]))
+        self.assertEqual(
+            district.overall_isp,
+            (850+625+400+250)/4000.0
+        )
+
+    def test_school(self):
+        s = CEPSchool( {
+            "District Name":"Test District",
+            "District Code":"TEST-D",
+            "School Name":"Test School",
+            "School Code": "TEST-S",
+            "total_enrolled": 1000,
+            "direct_cert": 100,
+            "foster": 100,
+            "homeless": 100,
+            "migrant": 100,
+        }) 
+        self.assertEqual(s.total_eligible,400)
+        self.assertEqual(s.isp,0.4)
+
+    def test_group(self):
+        pass
+
+
+class StrategyTestCase(unittest.TestCase,CEPTestMixin):
+    '''Runs through all registerd strategies and tests them. You can add custom per-strategy test
+    cases by setting up a test_districts data member
+    '''
+    def setUp(self):
+        self.test_districts = []
+        self.create_default_districts()
+        self.test_districts.append(self.create_default_districts())
 
     def test_strategies(self):
         for strategy_class in STRATEGIES.values():
@@ -45,3 +81,7 @@ class StrategyTestCase(unittest.TestCase):
                     self.assertEqual(sum([len(g.schools) for g in s.groups]),len(d.schools))
 
             # TODO how does a strategy define its expected result?
+
+
+
+
