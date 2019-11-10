43e1f560ba445bf56c9ad48c2430ee3430369633
Nikolaj Baer <nikolaj.baer@gmail.com>
2019-11-10 12:44:53 -0800

    added grouping to school listing, and made columns sortable

--- src/components/DistrictDetail.vue
+++ src/components/DistrictDetail.vue
@@ -29,19 +29,21 @@
             <table class="table col-sm">
               <thead class="thead-dark">
                 <tr>
-                  <th scope="col">School Code</th>
-                  <th scope="col">School Name</th>
+                  <th scope="col" @click="set_sort('grouping')">Recommended Grouping</th>
+                  <th scope="col" @click="set_sort('school_code')">School Code</th>
+                  <th scope="col" @click="set_sort('school_name')">School Name</th>
                   <th scope="col">School Type</th>
-                  <th scope="col">Total Enrolled</th>
+                  <th scope="col" @click="set_sort('total_enrolled')">Total Enrolled</th>
                   <th scope="col">Total Eligible</th>
                   <th scope="col">Daily Breakfast Served</th>
                   <th scope="col">Daily Lunch Served</th>
-                  <th scope="col">Included in Optimization</th>
-                  <th scope="col">Overall ISP</th>
+                  <th scope="col" @click="set_sort('active')">Included in Optimization</th>
+                  <th scope="col" @click="set_sort('isp')">Overall ISP</th>
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="school in district.data.schools" v-bind:key="school.code">
+                <tr v-for="school in ordered_schools" v-bind:key="school.code">
+                  <td>{{ (best_group_index!=null?best_group_index[school.school_code]:'n/a') }}</td>
                   <td>{{ school.school_code }}</td>
                   <td>{{ school.school_name }}</td>
                   <td>{{ school.school_type }}</td>
@@ -64,16 +66,42 @@ import * as _ from "lodash"
 // TODO "404" if no district?
 export default {
     props: ['state_code','district_code'],
+    data() {
+        return {
+            sort_col: "School Name",
+            sort_desc: false
+        }
+    },
     computed: {
         district(){
             var districts = _.filter(this.$store.getters.districts,d => d.code == this.district_code);
             if(districts.length){ return districts[0] }
             return null
         },
+        ordered_schools(){
+            if(this.district == null){ return [] }
+            const schools = this.district.data.schools;
+            schools.forEach( s => {
+                s.grouping = this.best_group_index[s.school_code];
+            })
+            return _.orderBy(schools, [this.sort_col], [this.sort_desc?"desc":"asc"]); 
+        },
         best_strategy(){
             if(this.district == null || this.district.data == null){ return null; }
             if(!this.district.data.strategies){ return null; }
             return this.district.data.strategies[this.district.data.best_index]
+        },
+        best_group_index(){
+            if(this.best_strategy == null){ return null; }
+            const group_index = {};
+            var i = 1;
+            this.best_strategy.groups.forEach( g => {
+                g.school_codes.forEach( sc => {
+                    group_index[sc] = i;
+                })
+                i += 1;
+            })
+            return group_index;
         }
     },
     watch: {
@@ -91,7 +119,15 @@ export default {
     methods: {
         loadDistrictData(){
             this.$store.dispatch("load_district", {code:this.district_code,state:this.state_code})
-        }
+        },
+        set_sort(col){
+            if(col == this.sort_col){
+              this.sort_desc = !this.sort_desc;
+            }else{
+              this.sort_col = col;
+              this.sort_desc = false;
+            }
+      }
     }
 }
 </script>
