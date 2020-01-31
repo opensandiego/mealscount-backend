aca5df9983ed8dc723d78ca62d041f84be75c616
Nikolaj Baer <nikolaj.baer@gmail.com>
2020-01-31 09:23:43 -0800

    adding missing redirect import

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect
 from flask_cors import CORS
 from flask_talisman import Talisman
 from werkzeug.routing import BaseConverter
