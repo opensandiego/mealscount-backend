a68dbe187571facde6a3848b4d1049ca4208e72a
Nikolaj Baer <nikolaj.baer@gmail.com>
2020-07-10 08:12:17 -0700

    updated copy changes from CFPA

--- src/components/district/DistrictDetail.vue
+++ src/components/district/DistrictDetail.vue
@@ -26,13 +26,13 @@
       <div class="row">
         <div class="col-sm alert alert-warning" role="alert">
           <strong>⚠️ PLEASE NOTE</strong>
-          <p>The data shown for this district is from the <a href="https://www.cde.ca.gov/ds/sd/sd/filescupc.asp" target="_blank">2019-2020 CALPADS CUPC source file</a> and the April 2019 
-          school meal claiminig data. The listed ISPs are calculated from direction certification through CalFresh/SNAP and CalWORKs only. Your current numbers may be significantly higher. </p>
- 
+          <p>The data shown for this district is from the publicly available <a href="https://www.cde.ca.gov/ds/sd/sd/filescupc.asp" target="_blank">2019-2020 CALPADS CUPC source file</a> and the April 2019 school meal claiming data. Due to limitations of the publicly available CALPADS data, the listed ISPs are calculated from direction certification through CalFresh/SNAP and CalWORKs only. Your actual current numbers may be significantly higher.</p>
+
           <p>Sites listed may include charter schools and exclude some preschool/early learning programs under your jurisdiction.</p>
- 
-          <p>To get the best recommended grouping, you should edit the school list as well as data affiliated with each school to better reflect current enrollment, 
-            eligibility, and meal participation. For more information, please <router-link to="/contact">Contact Us</router-link>!</p>
+
+          <p>To get the best recommended grouping, you should edit the school list as well as data affiliated with each school to better reflect current enrollment, eligibility, and meal participation.</p> 
+
+          <p>For more information, please <router-link to="/contact">Contact Us</router-link>!</p>
 
         </div>
       </div>
@@ -119,9 +119,9 @@
           <sup>1</sup>
           Based on {{ schoolDays }} days in school year
           <br />
-          <sup>2</sup> Derived from CALPADS CUPC report of students directly certified through participation in CalFresh/SNAP or CalWORKs
+          <sup>2</sup> Pre-loaded data is derived from publicly available CALPADS UPC report of students directly certified through participation in CalFresh/SNAP or CalWORKs.
           <br />
-          <sup>3</sup> Based on average daily participation for April 2019 calculated by CFPA from meal claim data provided by the California Department of Education
+          <sup>3</sup> Pre-loaded data is based on average daily participation for April 2019 calculated by CFPA from meal claim data provided by the California Department of Education.
           <br />
           </div>
       </div>
