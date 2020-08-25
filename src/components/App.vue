b93eac34edd47b45356735a75e313aa774a04146
nphiukhao <nphiukhao@gmail.com>
2020-08-25 15:47:15 -0700

    white spaces fixed

--- src/components/App.vue
+++ src/components/App.vue
@@ -19,11 +19,19 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav d-flex justify-content-end">
-          <router-link class="nav-item nav-link p-4" to="/explore">Find Your District</router-link>
+          <router-link class="nav-item nav-link p-4" to="/explore"
+            >Find Your District</router-link
+          >
           <router-link class="nav-item nav-link p-4" to="/faq">FAQ</router-link>
-          <router-link class="nav-item nav-link p-4" to="/contact">Contact</router-link>
-          <router-link class="nav-item nav-link p-4" to="/about">About</router-link>
-          <router-link class="nav-item nav-link p-4" to="/help">Help</router-link>
+          <router-link class="nav-item nav-link p-4" to="/contact"
+            >Contact</router-link
+          >
+          <router-link class="nav-item nav-link p-4" to="/about"
+            >About</router-link
+          >
+          <router-link class="nav-item nav-link p-4" to="/help"
+            >Help</router-link
+          >
         </div>
       </div>
     </nav>
@@ -36,25 +44,29 @@
     <router-view></router-view>
 
     <footer class="site-footer">
+      <div class="bottom-background gray" v-bind:style="footGrey" />
+      <div class="bottom-background" v-bind:style="footGreen" />
       <div class="nav-container px-3">
         <div class="row py-5">
           <div class="col-6 col-md">
             <h5>Resources</h5>
             <ul class="list-unstyled text-small">
               <li>
-                <a
-                  href="https://github.com/opensandiego/mealscount-backend/"
-                >Meals Count Source Code</a>
+                <a href="https://github.com/opensandiego/mealscount-backend/"
+                  >Meals Count Source Code</a
+                >
               </li>
               <li>
                 <a
                   href="https://www.fns.usda.gov/nslp/community-eligibility-provision-resource-center"
-                >USDA CEP Resource Center</a>
+                  >USDA CEP Resource Center</a
+                >
               </li>
               <li>
                 <a
                   href="https://github.com/opensandiego/mealscount-backend/blob/validate/CONTRIBUTING.md"
-                >Meals Count Contributing Guide</a>
+                  >Meals Count Contributing Guide</a
+                >
               </li>
               <li>
                 <router-link to="/faq">FAQ</router-link>
@@ -96,7 +108,9 @@
             <h5>About</h5>
             <ul class="list-unstyled text-small">
               <li>
-                <a href="https://github.com/opensandiego/mealscount-backend">Developers</a>
+                <a href="https://github.com/opensandiego/mealscount-backend"
+                  >Developers</a
+                >
               </li>
               <li>
                 <router-link to="/faq">FAQ</router-link>
@@ -111,7 +125,10 @@
               <!--<svg width="134" height="112">
                   <rect width="134" height="112" style="fill:rgb(255,255,255)" />
               </svg>-->
-              Meals Count
+              <div class="M">
+                <div class="footerM" v-bind:style="footerM" />
+              </div>
+              <p>eals Count</p>
             </div>
           </div>
         </div>
@@ -120,19 +137,61 @@
   </div>
 </template>
 
-<script >
+<script>
 import VTooltip from "v-tooltip";
 import MealsCountLogo from "../assets/MC_Logo@2x.png";
+import FOOTERM from "../assets/Union_2.svg";
+import FOOTERGREY from "../assets/grey-angle-two.svg";
+import FOOTERGREEN from "../assets/green-angle.svg";
 
 export default {
   data() {
     return {
-      logo: MealsCountLogo
+      logo: MealsCountLogo,
+      footerM: {
+        "background-image": "url('" + FOOTERM + "')",
+      },
+      footGrey: {
+        "background-image": "url('" + FOOTERGREY + "')",
+      },
+      footGreen: {
+        "background-image": "url('" + FOOTERGREEN + "')",
+      },
     };
   },
   mounted() {
     this.$store.dispatch("load_states");
     //this.$store.dispatch("load_districts","ca");
-  }
+  },
 };
 </script>
+
+
+<style scoped lang="scss">
+  .M {
+    width: 136px;
+    height: 120px;
+  }
+  .footerM {
+    width: 100%;
+    height: 100%;
+    background-repeat: no-repeat;
+  }
+  .icon p {
+    padding-top: 42px;
+    @media only screen and (min-width: 1500px) {
+      position: absolute;
+      left: 138px;
+    }
+  }
+  .bottom-background {
+    position: absolute;
+    width: 1930px;
+    height: 100%;
+    background-repeat: no-repeat;
+    visibility: hidden;
+    @media only screen and (min-width: 1400px) {
+      visibility: visible;
+    }
+} 
+</style> 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,142 +1,191 @@
 <template>
-    <section class="home">
-      <div class="homepage-hero position-relative overflow-hidden" v-bind:style="hero_style">
-        <div class="home-shadow">
+  <section class="home">
+    <div
+      class="homepage-hero position-relative overflow-hidden"
+      v-bind:style="hero_style"
+    >
+      <div class="home-shadow">
         <div class="overlay col-md-5 p-lg-5 my-5">
           <h1 class="display-4">Food for Students. Funds for Schools.</h1>
-          <hr/>
-          <p class="lead">Meals Count is a free tool to help school districts optimize their
-            Community Eligibility Provision (CEP) applications, maximize school meal funding, and fight childhood hunger.</p>
-           <router-link to="/faq" class="learn btn btn-outline-secondary">Learn More</router-link>
-           <router-link to="/explore" class="find filled btn btn-outline-secondary">Find Your District</router-link>
+          <hr />
+          <p class="lead">
+            Meals Count is a free tool to help school districts optimize their
+            Community Eligibility Provision (CEP) applications, maximize school
+            meal funding, and fight childhood hunger.
+          </p>
+          <router-link to="/faq" class="learn btn btn-outline-secondary"
+            >Learn More</router-link
+          >
+          <router-link
+            to="/explore"
+            class="find filled btn btn-outline-secondary"
+            >Find Your District</router-link
+          >
         </div>
+      </div>
+    </div>
+    <div class="centered-background gray" v-bind:style="graybg1" />
+    <div class="centered-background" v-bind:style="greenbg1" />
+    <div class="home-cards home-container">
+      <div class="row">
+        <h2 class="col-12 display-5">How Does Meals Count Work?</h2>
+        <hr class="fancy-rule" />
+      </div>
+
+      <div class="cards-container">
+        <div class="card-container p-2">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">LOCATE</h5>
+              <p class="card-text">Select your state and district.</p>
+              <img
+                v-bind:src="icons.locate"
+                alt="Select your state and district"
+                height="180px"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="card-container p-2">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">INPUT DATA</h5>
+              <p class="card-text">
+                Input your district's schools and school data
+              </p>
+              <img
+                v-bind:src="icons.inputdata"
+                alt="Input your district's schools and school data"
+                style="margin-top: 30px;"
+                height="103px"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="card-container p-2">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">RECOMMENDATIONS</h5>
+              <p class="card-text">
+                Get recommendations on how to group your schools for your CEP
+                Application
+              </p>
+              <img
+                v-bind:src="icons.recommendations"
+                alt="Get recommendations on how to group your schools for your CEP Application"
+                height="103px"
+              />
+            </div>
+          </div>
         </div>
       </div>
 
-      <div class="home-cards home-container">
-        <div class="centered-background gray" v-bind:style="graybg1"/>
-        <div class="centered-background" v-bind:style="greenbg1"/>
+      <div class="why-group row">
+        <div class="col-12">
+          <router-link class="grouping" to="/faq"
+            >Why does grouping matter?</router-link
+          >
+        </div>
+      </div>
+      <div class="get-started row">
+        <div class="col-12 text-center">
+          <router-link to="/explore" class="btn btn-primary outlined filled"
+            >Get Started</router-link
+          >
+        </div>
+      </div>
+    </div>
+
+    <div class="what-is-cep-block home-container">
+      <div class="row">
+        <h2 class="col-12">What Is CEP?</h2>
+        <hr class="fancy-rule" />
+      </div>
+      <div class="row content-row">
+        <div class="col-12 col-md-6 pb-4">
+          <p>
+            The Community Eligibility Provision (CEP) is a federally authorized
+            option for schools to serve and receive funding for breakfasts and
+            lunches that are free of charge to all students.
+          </p>
+
+          <p>
+            CEP helps reduce stigma, prevents student meal debt, and eliminates
+            the need to collect school meal applications. Nourishing students
+            through CEP helps all children learn, grow, and achieve at their
+            fullest potential.
+          </p>
+
+          <p>
+            <router-link to="/faq">Learn more</router-link>
+          </p>
+        </div>
+        <div class="col-12 col-md-6">
+          <img v-bind:src="worker_img" alt="CEP School Lunch" />
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <div class="support-block home-container text-center">
         <div class="row">
-          <h2 class="col-12 display-5">How Does Meals Count Work?</h2>
-          <hr class="fancy-rule"/>
+          <h2 class="col-12 display-5">
+            Meals Count is developed and supported by
+          </h2>
         </div>
-
-        <div class="cards-container">
-          <div class="card-container p-2">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">LOCATE</h5>
-                  <p class="card-text">Select your state and district.</p>
-                  <img v-bind:src="icons.locate" alt="Select your state and district" height="180px" />
-              </div>
-            </div>
-          </div>
-          <div class="card-container p-2">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">INPUT DATA</h5>
-                  <p class="card-text">Input your district's schools and school data</p>
-                  <img v-bind:src="icons.inputdata" alt="Input your district's schools and school data" style="margin-top: 30px;" height="103px" />
-              </div>
-            </div>
-          </div>
-          <div class="card-container p-2">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">RECOMMENDATIONS</h5>
-                  <p class="card-text">Get recommendations on how to group your schools for your CEP Application</p>
-                  <img v-bind:src="icons.recommendations" alt="Get recommendations on how to group your schools for your CEP Application" height="103px" />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="why-group row">
-          <div class="col-12">
-            <router-link class="grouping" to="/faq">Why does grouping matter?</router-link>
-          </div>
-        </div>
-        <div class="get-started row">
+        <div class="row">
           <div class="col-12 text-center">
-            <router-link to="/explore" class="btn btn-primary outlined filled">Get Started</router-link>
-          </div>
-        </div>
-
-      </div>
-
-      <div class="what-is-cep-block home-container">
-          <div class="row">
-            <h2 class="col-12">What Is CEP?</h2>
-            <hr class="fancy-rule"/>
-          </div>
-          <div class="row content-row">
-            <div class="col-12 col-md-6 pb-4">
-              <p>
-                The Community Eligibility Provision (CEP) is a federally authorized option for schools to serve and receive funding for breakfasts and lunches that are free of charge to all students.
-              </p>
-
-              <p>
-                CEP helps reduce stigma, prevents student meal debt, and eliminates the need to collect school meal applications. Nourishing students through CEP helps all children learn, grow, and achieve at their fullest potential.  
-              <p>
-                <router-link to="/faq">Learn more</router-link>
-              </p>
-            </div>
-            <div class="col-12 col-md-6">
-              <img v-bind:src="worker_img" alt="CEP School Lunch"/>
-            </div>
-          </div>
-      </div>
-
-      <div>
-        <div class="support-block home-container text-center">
-          <div class="row">
-            <h2 class="col-12 display-5">Meals Count is developed and supported by</h2>
-          </div>
-          <div class="row">
-            <div class="col-12 text-center">
-              <a target="_blank" href="https://opensandiego.org"><img class="logo" v-bind:src="logos.openSD_logo"></a>
-              <a target="_blank"  href='https://www.cfpa.net'> <img class='logo' v-bind:src="logos.cfpa_logo" /> </a>     
-              <a target="_blank"  href="https://www.sandiegohungercoalition.org/"> <img class="logo" v-bind:src="logos.hunger_logo">  </a>
-            </div>
+            <a target="_blank" href="https://opensandiego.org"
+              ><img class="logo" v-bind:src="logos.openSD_logo"
+            /></a>
+            <a target="_blank" href="https://www.cfpa.net">
+              <img class="logo" v-bind:src="logos.cfpa_logo" />
+            </a>
+            <a target="_blank" href="https://www.sandiegohungercoalition.org/">
+              <img class="logo" v-bind:src="logos.hunger_logo" />
+            </a>
           </div>
         </div>
       </div>
+    </div>
 
-      <div>
-        <div class="newsletter-block home-container text-center">
-          <div class="row">
-            <h2 class="col-12 display-5">Hear About Updates, Events, and New Features</h2>
-          </div>
-          <div class="row">
-            <div class="col-12 text-center">
-              <router-link to="newsletter/" class="btn btn-primary outlined filled">Sign Up For Our Newsletter</router-link>
-            </div>
+    <div>
+      <div class="newsletter-block home-container text-center">
+        <div class="row">
+          <h2 class="col-12 display-5">
+            Hear About Updates, Events, and New Features
+          </h2>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+            <router-link
+              to="newsletter/"
+              class="btn btn-primary outlined filled"
+              >Sign Up For Our Newsletter</router-link
+            >
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 </template>
 
-
 <script>
-import CFPALogo from '../assets/cfpalogo.png';
-import OPENSD from '../assets/openSD.jpeg';
-import HUNGER from '../assets/hunger.jpg';
-import HOMEPAGE_HERO from '../assets/homepage_hero2.jpg';
-import HOMEPAGE_WORKER from '../assets/Image_worker-food.png';
-import LOCATE_ICON from '../assets/locate-icon.svg';
-import INPUTDATA_ICON from '../assets/inputdata-icon.svg';
-import RECOMMENDATIONS_ICON from '../assets/recommendations-icon.svg';
-import CENTER_GREEN_BG from '../assets/gradient-swooper.svg';
-import CENTER_GRAY_BG from '../assets/center-gray.svg';
+import CFPALogo from "../assets/cfpalogo.png";
+import OPENSD from "../assets/openSD.jpeg";
+import HUNGER from "../assets/hunger.jpg";
+import HOMEPAGE_HERO from "../assets/homepage_hero2.jpg";
+import HOMEPAGE_WORKER from "../assets/Image_worker-food.png";
+import LOCATE_ICON from "../assets/locate-icon.svg";
+import INPUTDATA_ICON from "../assets/inputdata-icon.svg";
+import RECOMMENDATIONS_ICON from "../assets/recommendations-icon.svg";
+import CENTER_GREEN_BG from "../assets/gradient-swooper.svg";
+import CENTER_GRAY_BG from "../assets/center-gray.svg";
 
 export default {
   data() {
     return {
-      logos: { cfpa_logo: CFPALogo, 
-              openSD_logo: OPENSD,
-              hunger_logo: HUNGER
-      },
+      logos: { cfpa_logo: CFPALogo, openSD_logo: OPENSD, hunger_logo: HUNGER },
       icons: {
         locate: LOCATE_ICON,
         inputdata: INPUTDATA_ICON,
@@ -144,25 +193,27 @@ export default {
       },
       worker_img: HOMEPAGE_WORKER,
       hero_style: {
-        "background-image": "linear-gradient(90deg, rgb(0, 0, 0, .5) 0%, rgba(31, 31, 31, 0.2) 70%, rgba(84, 84, 84, 0) 100%), url('"+HOMEPAGE_HERO+"')",
-        backgroundSize: 'cover',
+        "background-image":
+          "linear-gradient(90deg, rgb(0, 0, 0, .5) 0%, rgba(31, 31, 31, 0.2) 70%, rgba(84, 84, 84, 0) 100%), url('" +
+          HOMEPAGE_HERO +
+          "')",
+        backgroundSize: "cover",
       },
       greenbg1: {
-        "background-image": "url('"+CENTER_GREEN_BG+"')",
+        "background-image": "url('" + CENTER_GREEN_BG + "')",
       },
       graybg1: {
-        "background-image": "url('"+CENTER_GRAY_BG+"')",
-      }
-
-    }
+        "background-image": "url('" + CENTER_GRAY_BG + "')",
+      },
+    };
   },
-  // From: https://forum.vuejs.org/t/how-do-i-add-remove-classes-to-body/1219/12 
+  // From: https://forum.vuejs.org/t/how-do-i-add-remove-classes-to-body/1219/12
   // .. not my favorite method, but need to fix home layout issue
   methods: {
     toggleBodyClass(addRemoveClass, className) {
       const el = document.body;
 
-      if (addRemoveClass === 'addClass') {
+      if (addRemoveClass === "addClass") {
         el.classList.add(className);
       } else {
         el.classList.remove(className);
@@ -170,16 +221,19 @@ export default {
     },
   },
   mounted() {
-    this.toggleBodyClass('addClass', 'body-home');
+    this.toggleBodyClass("addClass", "body-home");
   },
   destroyed() {
-    this.toggleBodyClass('removeClass', 'body-home');
-  }
-}
+    this.toggleBodyClass("removeClass", "body-home");
+  },
+};
 </script>
 
-
 <style scoped lang="scss">
+.home {
+  position: relative;
+  overflow: hidden;
+}
 .home-container {
   max-width: 1413px;
   margin: auto;
@@ -199,12 +253,12 @@ export default {
 }*/
 
 .centered-background {
-    position: absolute;
-    top: 900px;
-    width: 1930px;
-    left: -3px;
-    height: 100%;
-    background-repeat: no-repeat;
+  position: absolute;
+  top: 900px;
+  width: 1930px;
+  left: -3px;
+  height: 100%;
+  background-repeat: no-repeat;
 }
 
 .centered-background.gray {
@@ -213,18 +267,18 @@ export default {
   left: 424px;
 }
 
-
 section {
   color: white;
   text-align: left;
 }
 
-p.lead, h1 {
-    text-align: left;
-    font-weight: bold;
-    font-size: 55px;
-    color: white;
-    font-family: "century-gothic";
+p.lead,
+h1 {
+  text-align: left;
+  font-weight: bold;
+  font-size: 55px;
+  color: white;
+  font-family: "century-gothic";
 }
 
 p.lead {
@@ -246,34 +300,31 @@ hr {
   width: 120px;
 }
 
-
 .btn {
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  border: 3px solid #F27C3E;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border: 3px solid #f27c3e;
   border-radius: 31px;
   text-align: center;
   font: Bold 22px/40px century-gothic;
   letter-spacing: 0.02px;
-  color: #F27C3E;
+  color: #f27c3e;
   opacity: 1;
   width: 260px;
   height: 75;
-
 }
 
 .btn.learn {
   margin-right: 70px;
   &:hover {
-    color: #FFFFFF;
-    background: #F27C3E;
+    color: #ffffff;
+    background: #f27c3e;
   }
 }
 .btn.find {
   margin-right: 0px;
 }
 
-.homepage-hero .btn{
-
+.homepage-hero .btn {
   @media only screen and (max-width: 1600px) {
     display: block;
     margin: 20px auto;
@@ -283,18 +334,19 @@ hr {
 }
 .homepage-hero {
   margin: 0px;
+  z-index: 1;
 }
 .btn.filled {
-  background: #F27C3E 0% 0% no-repeat padding-box;
-  color: #FFFFFF;
-      &:hover {
-    color: #F27C3E;
-    background: #FFFFFF;
+  background: #f27c3e 0% 0% no-repeat padding-box;
+  color: #ffffff;
+  &:hover {
+    color: #f27c3e;
+    background: #ffffff;
   }
 }
 .fancy-rule {
-  border: 3px solid #B0D35C;;
-  margin-bottom:  102px;
+  border: 3px solid #b0d35c;
+  margin-bottom: 102px;
   width: 120px;
 }
 
@@ -304,7 +356,7 @@ hr {
   margin: 100px auto;
 
   .cards-container {
-    @media only screen and (min-width: 1000px){
+    @media only screen and (min-width: 1000px) {
       display: flex;
       justify-content: space-between;
     }
@@ -322,7 +374,7 @@ hr {
     /* need to pull this from bootstrap */
     @media only screen and (max-width: 1024px) {
       width: 300px;
-      height:430px;
+      height: 430px;
     }
   }
   .card-body {
@@ -338,7 +390,7 @@ hr {
       font-size: 20px;
       img {
         left: 95px;
-        bottom:55px;
+        bottom: 55px;
       }
     }
   }
@@ -352,14 +404,16 @@ hr {
     }
   }
 
-
   h2 {
     font-size: 45px;
   }
   .why-group {
     margin-top: 31px;
     font-size: 24px;
-    a { color: white; text-decoration: underline; }
+    a {
+      color: white;
+      text-decoration: underline;
+    }
   }
   .get-started {
     margin-top: 72px;
@@ -370,12 +424,12 @@ hr {
 .what-is-cep-block {
   h2 {
     text-align: center;
-    font-size: 45px; 
+    font-size: 45px;
   }
   .content-row {
     margin: 0px 10px 268px 10px;
     font-size: 31px;
-    @media only screen and (max-width: 1000px){
+    @media only screen and (max-width: 1000px) {
       font-size: 25px;
     }
   }
