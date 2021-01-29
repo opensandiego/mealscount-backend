236f0187116a56a2e025f719b5dce11f759cbbb7
Nikolaj Baer <nikolaj.baer@gmail.com>
2021-01-29 14:41:04 -0800

    state specific faq and content for CA

---
+++ src/components/ContactDisplay.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="contact">
+
+        <div v-if="contact.agency">
+            <h4>State</h4>
+            <p>
+                <a target="_blank" :href="contact.agency.cep_website">
+                    {{ contact.agency.name }}
+                </a>
+            </p>
+            <p v-for="phone in contact.agency.cep_phone" v-bind:key="phone">
+                <a :href="'tel:'+phone">{{ phone }}</a>
+            </p>
+            <p v-for="email in contact.agency.cep_email" v-bind:key="email">
+                <a :href="'mailto:'+email">{{ email }}</a>
+            </p>
+    </div>
+
+        <div v-if="contact.organizations.length > 0">
+            <h4 >Organizations</h4>
+            <ul>
+                <li v-for="org in contact.organizations" v-bind:key="org.name">
+                    <a target="_blank" :href="org.website">
+                        {{ org.name }}
+                    </a><br>
+                    <p v-for="phone in org.phone" v-bind:key="phone">
+                        <a :href="'tel:'+phone">{{ phone }}</a>
+                    </p>
+                    <p v-for="email in org.email" v-bind:key="email">
+                        <a :href="'mailto:'+email">{{ email }}</a>
+                    </p>
+                    <p v-for="phone in org.cep_phone" v-bind:key="phone">
+                        CEP Specific: <a :href="'tel:'+phone">{{ phone }}</a>
+                    </p>
+                    <p v-for="email in org.cep_email" v-bind:key="email">
+                        CEP Specific: <a :href="'mailto:'+email">{{ email }}</a>
+                    </p>
+                </li>
+            </ul>
+        </div>
+
+        <div v-if="contact.additional" v-html="contact.additional"></div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["contact"],
+}
+</script>
---
+++ src/components/StateFaq.vue
@@ -0,0 +1,54 @@
+<template>
+ <section class="container mt-5">
+    <router-link :to="{name:'faq'}">&laquo; Back to FAQ</router-link>
+
+    <h1 class="headline text-center mb-4">Meals Count FAQ - {{ state.name }}</h1>
+
+
+    <div class="faq" v-html="state.faq"></div>
+
+    <div class="card" v-if="state.contact">
+        <div class="card-header">
+            <h2 id="contact">Contact Information for {{ state.name }}</h2>
+        </div>
+        <div class="card-body">
+            <ContactDisplay v-if="state.contact"  v-bind:contact="state.contact" />
+        </div>
+    </div>
+
+    <div class="district-link center m-4 text-center">
+        <router-link class="btn btn-primary" :to="{name:'state-detail', params: {state_code:state_code} }" >
+            View Districts in {{ state.name }}
+        </router-link>
+    </div>
+
+ </section>
+</template>
+
+
+<script>
+import ContactDisplay from "./ContactDisplay.vue";
+
+export default {
+    components:{ContactDisplay},
+    props: ["state_code"],
+    computed: {
+        state() {
+            return this.$store.getters.get_state(this.state_code);
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+.district_link {
+    margin: 10px auto;
+    text-align: center;
+}
+h3 {
+  margin-top: 40px;
+}
+
+</style>
+
