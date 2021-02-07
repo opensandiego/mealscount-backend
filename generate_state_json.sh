e8e2e214d4404357ceffbc060f03cb3538f434c7
Nikolaj Baer <nikolaj.baer@gmail.com>
2021-02-07 12:19:15 -0800

    fix issue in generate state json, simplify docker file for build

--- generate_state_json.sh
+++ generate_state_json.sh
@@ -1,6 +1,10 @@
 # Builds json district data in /static/dist/ based on data/*/latest.csv
 
 for d in `ls data`; 
-    do python cep_estimatory.py data/$d/latest.csv --output-folder dist/static/$d/;
+do
+    if [ -d $d ]
+    then
+        python cep_estimatory.py data/$d/latest.csv --output-folder dist/static/$d/;
+    fi
 done;
 
