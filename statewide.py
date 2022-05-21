172f1138a294dfbe0ec47c7c88f173de27e5eda4
Nikolaj Baer <nikolaj.baer@gmail.com>
2022-05-21 10:50:19 -0700

    add ability to specify group max

--- statewide.py
+++ statewide.py
@@ -12,8 +12,8 @@ STRATEGIES = [
   "OneGroup",
   "Spread",
   "Binning",
-  "NYCMODA?fresh_starts=50&iterations=1000&ngroups=10",
-  "GreedyLP"
+  "GreedyLP",
+  "NYCMODA?fresh_starts=50&iterations=1000&ngroups=%(ngroups)s",
 ]
 
 @click.command()
@@ -21,7 +21,8 @@ STRATEGIES = [
 @click.option("--state",default=None,help="State Abbrev (e.g. CA, NY)")
 @click.option("--csv-encoding",default="utf-8",help="CSV Encoding (e.g. utf-8, latin1)")
 @click.option("--debug",is_flag=True,help="Run a quick test run on districts of < 5 schools just to test")
-def run(csv_file,state,csv_encoding,debug):
+@click.option("--max-groups",default=10,help="Paramter for max groups limiter on monte carlo")
+def run(csv_file,state,csv_encoding,debug,max_groups):
 
   # Load
   districts,schools,lastyear_groupings = load_from_csv(csv_file,csv_encoding,state)
@@ -37,9 +38,10 @@ def run(csv_file,state,csv_encoding,debug):
     districts = {c:d for c,d in districts.items() if len(d.schools) <= 5}
 
   # Optimize with max reimbursement
-  results = optimize(districts)
+  strategies = [s%{"ngroups":max_groups} for s in STRATEGIES]
+  results = optimize(districts,strategies)
   # TODO optimize with max coverage
-  results_coverage = optimize(districts,goal="coverage")
+  results_coverage = optimize(districts,strategies,goal="coverage")
 
   # Run Naive Baselines
   for d in districts.values():
@@ -58,7 +60,7 @@ def run(csv_file,state,csv_encoding,debug):
   for r in results:
     district_results[r["code"]] = {"reimb": r["reimb"],"best_reimb":r["best"]}
   for r in results_coverage:
-    district_results[r["code"]]["coverage"] = r["coverage"]
+    #district_results[r["code"]]["coverage"] = r["coverage"]
     district_results[r["code"]]["best_coverage"] = r["best"]
   for d in districts.values():
     district_results[d.code]["onegroup_reimb"] = d.strategies[0].reimbursement
@@ -69,6 +71,8 @@ def run(csv_file,state,csv_encoding,debug):
     district_results[d.code]["lastyear_coverage"] = d.strategies[2].students_covered
 
   def deltapercent(x,y,explain):
+    if y == 0 and x > 0: return "100%"
+    elif y == 0 and x == 0: return "0%"
     diff = (((x-y)/y)*100.0)
     return "%0.1f%% %s %s" % (diff,diff>0 and "increase" or "decrease",explain)
 
@@ -94,14 +98,19 @@ def output(districts,results,state,lastyear_groupings):
       "school_name",
       "total_enrolled",
       "total_eligible",
+      "daily_breakfast_served",
+      "daily_lunch_served",
+      "free_bfast_rate",
+      "paid_bfast_rate",
+      "free_lunch_rate",
+      "paid_lunch_rate",
       "onegroup_reimbursement",
       "onetoone_reimbursement",
       "lastyear_reimbursement",
       "mealscount_reimbursement",
+      "mealscount_coverage",
       "lastyear_grouping",
       "mealscount_grouping",
-#      "lunch_rate",
-#      "breakfast_rate",
     ))
     for d in districts.values():
       school_results = [r for r in results if r["code"] == d.code][0]["schools"]
@@ -109,6 +118,7 @@ def output(districts,results,state,lastyear_groupings):
         lastyear_group = [g[1] for g in lastyear_groupings if g[2] == s.code and g[0] == d.code]
         if lastyear_group: lastyear_group = lastyear_group[0]
         else: lastyear_group = ""
+        sr = [sr for sr in school_results if sr["school_code"] == s.code][0]
         writer.writerow((
           d.code,
           d.name,
@@ -116,12 +126,19 @@ def output(districts,results,state,lastyear_groupings):
           s.name,
           s.total_enrolled,
           s.total_eligible,
+          s.bfast_served,
+          s.lunch_served,
+          sr["rates"]["free_bfast"],
+          sr["rates"]["paid_bfast"],
+          sr["rates"]["free_lunch"],
+          sr["rates"]["paid_lunch"],
           d.strategies[0].school_reimbursement(s),
           d.strategies[1].school_reimbursement(s),
           d.strategies[2].school_reimbursement(s),
-          [sr for sr in school_results if sr["school_code"] == s.code][0]["reimbursement"],
+          sr["reimbursement"],
+          sr["coverage"],
           lastyear_group,
-          [sr for sr in school_results if sr["school_code"] == s.code][0]["grouping"],
+          sr["grouping"],
         ))
 
 def load_from_csv(csv_file,csv_encoding,state):
@@ -140,15 +157,16 @@ def load_from_csv(csv_file,csv_encoding,state):
         districts[row["district_code"]].add_school(school)
   return districts,schools,lastyear_groupings
 
-def optimize(districts,goal="reimbursement"):
+def optimize(districts,strategies,goal="reimbursement"):
   # Optimize with standard Strategies
+  print("optimizing with %s" % (','.join(strategies)))
   district_map = dict([(d.code,d) for d in districts.values()])
   t0 = time.time()
   # We use multiprocessing to speed this all up
   PROCESSES = multiprocessing.cpu_count() - 1
   with click.progressbar(length=sum([len(d.schools) for d in districts.values()]),label='Running Strategies on Districts') as bar:
     with multiprocessing.Pool(PROCESSES) as pool:
-        results = [pool.apply_async(mp_processor, (d,goal)) for d in districts.values()]
+        results = [pool.apply_async(mp_processor, (d,goal,strategies)) for d in districts.values()]
         for r in results:
             result = r.get()
             _d = district_map[result["code"]]
@@ -157,24 +175,30 @@ def optimize(districts,goal="reimbursement"):
   print("Optimized in %0.1fs" % total_time)
   return [r.get() for r in results]
  
-def mp_processor(district,goal):
-  add_strategies(district,*STRATEGIES)
+def mp_processor(district,goal,strategies):
+  add_strategies(district,*strategies)
   district.run_strategies()
   district.evaluate_strategies(evaluate_by=goal)
   schools = []
+  gname = 1
   for g in district.best_strategy.groups:
     for s in g.schools:
+      reimb = g.school_reimbursement(s)
+      s.set_rates(district)
       schools.append({
         "school_code":s.code,
+        "rates":s.rates.as_dict(),
         "district_code":district.code,
-        "reimbursement": g.school_reimbursement(s),
-        "grouping": g.name,
+        "reimbursement": reimb,
+        "coverage": g.cep_eligible and s.total_enrolled or 0,
+        "grouping": g.cep_eligible and "G%i" % gname or "Not CEP Eligible",
       })
+    if g.cep_eligible:
+      gname += 1
   
   return {
       "code":district.code,
       "reimb":district.best_strategy.reimbursement,
-      "coverage":district.best_strategy.students_covered,
       "best":district.best_strategy.name,
       "groupings": [g.as_dict() for g in district.best_strategy.groups],
       "schools": schools,
